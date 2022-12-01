13b429ff630b5a418c08136d480dfed4b77fbfe8
Biswajeet Sahoo <97235820+Biswajeet78@users.noreply.github.com>
2022-12-01 12:48:48 +0530

    Add files via upload

---
+++ routes/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const router = express.Router();
+
+console.log('router is loaded');
+
+//to handle all routes of /doctors url
+router.use('/doctors', require('./doctor'));
+
+//to handle all routes od /patient url
+router.use('/patients', require('./patient'));
+
+//to handle all routes od /report url
+router.use('/report', require('./report'));
+
+
+module.exports = router;
