cf3a22490ad08488083c9d22392dd792e5532c6a
Biswajeet Sahoo <97235820+Biswajeet78@users.noreply.github.com>
2022-12-01 12:45:18 +0530

    Add files via upload

---
+++ routes/doctor.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+const doctorController = require('../controllers/doc_controller')
+
+
+router.post('/register',doctorController.register);
+router.post('/login',doctorController.login);
+
+
+
+module.exports = router;
